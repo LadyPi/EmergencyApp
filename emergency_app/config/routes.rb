282c20be9f3e2abbd68eb8eb53a17add7b5a2b26
LadyPi <ms.vanessa.farias.dm@gmail.com>
2016-07-16 22:24:44 -0700

    creates initial routes: index, about, sign-up, user profile, log in, session, to do, contact

--- emergency_app/config/routes.rb
+++ emergency_app/config/routes.rb
@@ -1,9 +1,136 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  
+  ########################################################################
+ 
+  ##Home/Launch Page/New Message##
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # Index/Home Page
+  root to: 'index#show'
+
+  # New Message 
+  POP UP? TEST
+  AJAX?
+  TEST
+
+  ########################################################################
+
+  ##Service Overview##
+  
+  # About Service/How To Use Service Page
+  get '/about', to: 'about#show'
+
+  ########################################################################
+
+ ##USERS: Sign-Up/Profile##
+
+  # New User Profile Page
+  get '/signup', to: 'users#new'
+
+  # Create User Profile Page
+  post '/users', to: 'users#create'
+
+  # Show User Profile Page
+  get '/users/:id', to: 'users#show'
+
+  (can you show and edit on same page)
+
+  # Edit User Profile Page
+  get '/users/:id/edit', to: 'users#edit'
+
+   # Delete User Profile Page
+  delete '/users/:id', to: 'users#destroy'
+
+  ########################################################################
+
+ ##USERS: Log In/Session##
+
+  # New User Session
+  get '/signin', to: 'sessions#new'
+
+  # Create User Session
+  post '/sessions', to: 'sessions#create'
+
+  why no id?
+
+  ##########################################
+  
+  ##USERS: Log Out/Session##
+
+  # Terminate User Session
+  delete '/sessions', to: 'sessions#destroy'
+
+  why no id?
+  On Click 'Log Out'
+
+  ########################################################################
+
+  ##Emergency List Of To Dos##
+  
+  # New Emergency To Do List Page
+  get '/todos', to: 'todos#new'
+
+  # Create Emergency To Do List Page
+  post '/todos', to: 'todos#create'
+
+  # Show Emergency To Do List Page
+  get '/todos/:id', to: 'todos#show'
+
+  # Edit Emergency To Do List Page
+  get '/todos/:id/edit', to: 'todos#edit'
+
+  # Update Section Of Emergency To Do List Page
+  patch '/todos/:id', to: 'todos#update'
+
+  # Delete Emergency To Do List Page
+  delete '/todos/:id', to: 'todos#destroy'
+
+  ########################################################################
+
+  ##Emergency Titles##
+  
+  DO NOT need CRUD for this: can simply render titles to click on 
+  and make sure disappear when deleted in get '/todos/:id', to: 'todos#show'
+  
+  ########################################################################
+
+  ##Emergency Contacts##
+
+  # New Emergency Contacts
+  get '/contacts', to: 'contacts#new'
+
+  # Create Emergency Contacts
+  post '/contacts', to: 'contacts#create'
+  
+  # Show Emergency Contacts
+  get '/contacts/:id', to: 'contacts#show'
+
+  # Edit Emergency Contacts
+  get '/contacts/:id/edit', to: 'contacts#edit'
+
+  # Delete Emergency Contacts
+  delete '/contacts/:id', to: 'contacts#destroy'
+
+end
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -53,4 +180,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
