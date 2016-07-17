44684c81913bcc83c908592c81ae9897fc111bd3
LadyPi <ms.vanessa.farias.dm@gmail.com>
2016-07-16 22:58:35 -0700

    creates initial controllers: about, contacts, lists, sessions, titles, todos, users; additional: assets, helpers

--- emergency_app/config/routes.rb
+++ emergency_app/config/routes.rb
@@ -7,110 +7,113 @@ Rails.application.routes.draw do
   ##Home/Launch Page/New Message##
 
   # Index/Home Page
-  root to: 'index#show'
+ #  root to: 'index#show'
 
-  # New Message 
-  POP UP? TEST
-  AJAX?
-  TEST
+ #  # New Message 
+ #  POP UP? TEST
+ #  AJAX?
+ #  TEST
 
-  ########################################################################
+ #  ########################################################################
 
-  ##Service Overview##
+ #  ##Service Overview##
   
-  # About Service/How To Use Service Page
-  get '/about', to: 'about#show'
+ #  # About Service/How To Use Service Page
+ #  get '/about', to: 'about#show'
 
-  ########################################################################
+ #  ########################################################################
 
- ##USERS: Sign-Up/Profile##
+ # ##USERS: Sign-Up/Profile##
 
-  # New User Profile Page
-  get '/signup', to: 'users#new'
+ #  # New User Profile Page
+ #  get '/signup', to: 'users#new'
 
-  # Create User Profile Page
-  post '/users', to: 'users#create'
+ #  # Create User Profile Page
+ #  post '/users', to: 'users#create'
 
-  # Show User Profile Page
-  get '/users/:id', to: 'users#show'
+ #  # Show User Profile Page
+ #  get '/users/:id', to: 'users#show'
 
-  (can you show and edit on same page)
+ #  (can you show and edit on same page)
 
-  # Edit User Profile Page
-  get '/users/:id/edit', to: 'users#edit'
+ #  # Edit User Profile Page
+ #  get '/users/:id/edit', to: 'users#edit'
 
-   # Delete User Profile Page
-  delete '/users/:id', to: 'users#destroy'
+ #   # Delete User Profile Page
+ #  delete '/users/:id', to: 'users#destroy'
 
-  ########################################################################
+ #  ########################################################################
 
- ##USERS: Log In/Session##
+ # ##USERS: Log In/Session##
 
-  # New User Session
-  get '/signin', to: 'sessions#new'
+ #  # New User Session
+ #  get '/signin', to: 'sessions#new'
 
-  # Create User Session
-  post '/sessions', to: 'sessions#create'
+ #  # Create User Session
+ #  post '/sessions', to: 'sessions#create'
 
-  why no id?
+ #  why no id?
 
-  ##########################################
+ #  ##########################################
   
-  ##USERS: Log Out/Session##
+ #  ##USERS: Log Out/Session##
 
-  # Terminate User Session
-  delete '/sessions', to: 'sessions#destroy'
+ #  # Terminate User Session
+ #  delete '/sessions', to: 'sessions#destroy'
 
-  why no id?
-  On Click 'Log Out'
+ #  why no id?
+ #  On Click 'Log Out'
 
-  ########################################################################
+ #  ########################################################################
 
-  ##Emergency List Of To Dos##
+ #  ##Emergency List Of To Dos##
   
-  # New Emergency To Do List Page
-  get '/todos', to: 'todos#new'
+ #  # New Emergency To Do List Page
+ #  get '/todos', to: 'todos#new'
 
-  # Create Emergency To Do List Page
-  post '/todos', to: 'todos#create'
+ #  # Create Emergency To Do List Page
+ #  post '/todos', to: 'todos#create'
 
-  # Show Emergency To Do List Page
-  get '/todos/:id', to: 'todos#show'
+ #  # Show Emergency To Do List Page
+ #  get '/todos/:id', to: 'todos#show'
 
-  # Edit Emergency To Do List Page
-  get '/todos/:id/edit', to: 'todos#edit'
+ #  # Edit Emergency To Do List Page
+ #  get '/todos/:id/edit', to: 'todos#edit'
 
-  # Update Section Of Emergency To Do List Page
-  patch '/todos/:id', to: 'todos#update'
+ #  # Update Section Of Emergency To Do List Page
+ #  patch '/todos/:id', to: 'todos#update'
 
-  # Delete Emergency To Do List Page
-  delete '/todos/:id', to: 'todos#destroy'
+ #  # Delete Emergency To Do List Page
+ #  delete '/todos/:id', to: 'todos#destroy'
 
-  ########################################################################
+ #  ########################################################################
 
-  ##Emergency Titles##
+ #  ##Emergency Titles##
+ 
+ #  Show ALL Emergency Titles
+ #  get '/titles', to: 'titles#show'
+ 
+ #  DO NOT need CRUD for this: can simply render titles to click on 
+ #  and make sure disappear when deleted in get '/todos/:id', to: 'todos#show'
   
-  DO NOT need CRUD for this: can simply render titles to click on 
-  and make sure disappear when deleted in get '/todos/:id', to: 'todos#show'
+ #  ########################################################################
+
+ #  ##Emergency Contacts##
+
+ #  # New Emergency Contacts
+ #  get '/contacts', to: 'contacts#new'
+
+ #  # Create Emergency Contacts
+ #  post '/contacts', to: 'contacts#create'
   
-  ########################################################################
+ #  # Show Emergency Contacts
+ #  get '/contacts/:id', to: 'contacts#show'
 
-  ##Emergency Contacts##
+ #  # Edit Emergency Contacts
+ #  get '/contacts/:id/edit', to: 'contacts#edit'
 
-  # New Emergency Contacts
-  get '/contacts', to: 'contacts#new'
-
-  # Create Emergency Contacts
-  post '/contacts', to: 'contacts#create'
-  
-  # Show Emergency Contacts
-  get '/contacts/:id', to: 'contacts#show'
-
-  # Edit Emergency Contacts
-  get '/contacts/:id/edit', to: 'contacts#edit'
-
-  # Delete Emergency Contacts
-  delete '/contacts/:id', to: 'contacts#destroy'
+ #  # Delete Emergency Contacts
+ #  delete '/contacts/:id', to: 'contacts#destroy'
 
 end
   
--- emergency_app/db/schema.rb
+++ emergency_app/db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160716233243) do
+ActiveRecord::Schema.define(version: 20160717053300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "cell_phone_number"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string   "name"
